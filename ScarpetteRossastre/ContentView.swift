8ffb1b07907660cda2f389b002f25841f8194b4e
Antonio Castaldo <ancastal@libero.it>
2021-12-03 10:57:03 +0100

    Initial Commit

---
+++ ScarpetteRossastre/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  ScarpetteRossastre
+//
+//  Created by Antonio Castaldo on 03/12/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ ScarpetteRossastre/ScarpetteRossastreApp.swift
@@ -0,0 +1,17 @@
+//
+//  ScarpetteRossastreApp.swift
+//  ScarpetteRossastre
+//
+//  Created by Antonio Castaldo on 03/12/21.
+//
+
+import SwiftUI
+
+@main
+struct ScarpetteRossastreApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
