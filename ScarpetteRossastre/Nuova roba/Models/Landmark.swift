6350cba80e20a8c3c426fbcd7d23f5d3e4474964
Antonio Castaldo <ancastal@libero.it>
2021-12-06 15:35:48 +0100

    Update

--- ScarpetteRossastre/Nuova roba/Models/Landmark.swift
+++ ScarpetteRossastre/Nuova roba/Models/Landmark.swift
@@ -16,20 +16,20 @@ struct Landmark: Hashable, Codable, Identifiable {
     var state: String
     var description: String
 
-//    private var imageName: String
-//    var image: Image {
-//        Image(imageName)
-//    }
-//
-//    private var coordinates: Coordinates
-//    var locationCoordinate: CLLocationCoordinate2D {
-//        CLLocationCoordinate2D(
-//            latitude: coordinates.latitude,
-//            longitude: coordinates.longitude)
-//    }
-//
-//    struct Coordinates: Hashable, Codable {
-//        var latitude: Double
-//        var longitude: Double
-//    }
+    private var imageName: String
+    var image: Image {
+        Image(imageName)
+    }
+
+    private var coordinates: Coordinates
+    var locationCoordinate: CLLocationCoordinate2D {
+        CLLocationCoordinate2D(
+            latitude: coordinates.latitude,
+            longitude: coordinates.longitude)
+    }
+
+    struct Coordinates: Hashable, Codable {
+        var latitude: Double
+        var longitude: Double
+    }
 }
--- ScarpetteRossastre/Views/MapView.swift
+++ ScarpetteRossastre/Views/MapView.swift
@@ -58,10 +58,10 @@ struct Home: View{
                 
                 //            Map(coordinateRegion: $region, interactionModes: .all, showsUserLocation: true, userTrackingMode: $tracking)
                 
-                Map(coordinateRegion: $managerDelegate.region, interactionModes: .all, showsUserLocation: true, userTrackingMode: $tracking, annotationItems: managerDelegate.pins) { pin in
+                Map(coordinateRegion: $managerDelegate.region, interactionModes: .all, showsUserLocation: true, userTrackingMode: $tracking, annotationItems: managerDelegate.lands) { pin in
                     //                    MapPin(coordinate: pin.location.coordinate, tint: .green) //  <--- Pin standard
                     
-                    MapAnnotation(coordinate: pin.location.coordinate) {
+                    MapAnnotation(coordinate: pin.locationCoordinate) {
                         VStack {                                       //<---       Pin personalizzato con TapGesture
                             Image(systemName: "mappin.circle.fill")
                                 .font(.system(size: 30))
@@ -74,10 +74,10 @@ struct Home: View{
                         //                                                               var location = coordinates + 2
                         //                                                               if (coordinates...location).contains(pin){aggiungi a modale pin.info}
                         .onTapGesture {
-                            indirizzoPin = pin.indirizzo
-                            nomeAssociazionePin = pin.nomeAssociazione
-                            numeroTelefonoPin = pin.numeroTelefono
-                            descrizionePin = pin.descrizione
+                            indirizzoPin = pin.name
+//                            nomeAssociazionePin = pin.nomeAssociazione
+//                            numeroTelefonoPin = pin.numeroTelefono
+//                            descrizionePin = pin.descrizione
                         }
                         
                     } //.edgesIgnoringSafeArea(.all)
@@ -117,6 +117,7 @@ struct Home: View{
 
 class locationDelegate: NSObject,ObservableObject,CLLocationManagerDelegate{
     var pins : [Pin] = []
+    var lands : [Landmark] = []
     // From here and down is new
     @Published var location: CLLocation?
     @State var hasSetRegion = false
@@ -145,39 +146,40 @@ class locationDelegate: NSObject,ObservableObject,CLLocationManagerDelegate{
         
         
         //        Creo array myPin
-        let myPin: [Pin] = [
-            Pin(location: CLLocation(latitude:40.90730440345204, longitude:14.75453973651237),
-                nomeAssociazione: "Centro Antiviolenza PdZ A02",
-                indirizzo: "Via Nazionale 222 presso il Centro Sociale P. Campanello 80013 - Mercogliano (AV)",
-                numeroTelefono: "0825-682501",
-                descrizione: "Centro antiviolenza"),
-            Pin(location: CLLocation(latitude:41.039990244809694, longitude:14.382515760918347),
-                nomeAssociazione: "Cooperativa Eva" ,
-                indirizzo: "Via G. Amendola, 15, 81024 Maddaloni CE",
-                numeroTelefono: "082-3204145",
-                descrizione: "Informazioni sulla Cooperativa Eva"),
-            Pin(location: CLLocation(latitude:40.944596372450285, longitude:14.36566292163162),
-                nomeAssociazione: "La Casa di Marinella",
-                indirizzo: "Via Volturno, 71, 80011 Acerra NA",
-                numeroTelefono: "081-5201470",
-                descrizione: "Informazioni su La Casa di Marinella"),
-            Pin(location: CLLocation(latitude:40.85258986902009, longitude:14.333050897689262),
-                nomeAssociazione: "Sportello Donna Le Kassandre",
-                indirizzo: "Corso Ponticelli, 40, 80147 Napoli NA",
-                numeroTelefono: "388-0979950",
-                descrizione: "Informazioni su Sportello Donna Le Kassandre"),
-            Pin(location: CLLocation(latitude:40.84578935394306, longitude:14.36968345635139),
-                nomeAssociazione: "Sportello Lilith",
-                indirizzo: "Via Luca Giordano, 24, 80040 San Sebastiano Al Vesuvio NA",
-                numeroTelefono: "331-9021391",
-                descrizione: "Informazioni su Sportello Lilith")
-        ]
+//        let myPin: [Pin] = [
+//            Pin(location: CLLocation(latitude:40.90730440345204, longitude:14.75453973651237),
+//                nomeAssociazione: "Centro Antiviolenza PdZ A02",
+//                indirizzo: "Via Nazionale 222 presso il Centro Sociale P. Campanello 80013 - Mercogliano (AV)",
+//                numeroTelefono: "0825-682501",
+//                descrizione: "Centro antiviolenza"),
+//            Pin(location: CLLocation(latitude:41.039990244809694, longitude:14.382515760918347),
+//                nomeAssociazione: "Cooperativa Eva" ,
+//                indirizzo: "Via G. Amendola, 15, 81024 Maddaloni CE",
+//                numeroTelefono: "082-3204145",
+//                descrizione: "Informazioni sulla Cooperativa Eva"),
+//            Pin(location: CLLocation(latitude:40.944596372450285, longitude:14.36566292163162),
+//                nomeAssociazione: "La Casa di Marinella",
+//                indirizzo: "Via Volturno, 71, 80011 Acerra NA",
+//                numeroTelefono: "081-5201470",
+//                descrizione: "Informazioni su La Casa di Marinella"),
+//            Pin(location: CLLocation(latitude:40.85258986902009, longitude:14.333050897689262),
+//                nomeAssociazione: "Sportello Donna Le Kassandre",
+//                indirizzo: "Corso Ponticelli, 40, 80147 Napoli NA",
+//                numeroTelefono: "388-0979950",
+//                descrizione: "Informazioni su Sportello Donna Le Kassandre"),
+//            Pin(location: CLLocation(latitude:40.84578935394306, longitude:14.36968345635139),
+//                nomeAssociazione: "Sportello Lilith",
+//                indirizzo: "Via Luca Giordano, 24, 80040 San Sebastiano Al Vesuvio NA",
+//                numeroTelefono: "331-9021391",
+//                descrizione: "Informazioni su Sportello Lilith")
+//        ]
         
         pins = []
+
         //        pins.append(Pin(location:locations.last!))            Pin GPS
         
-        for pin in myPin {
-            pins.append(pin)
+        for land in landmarks {
+            lands.append(land)
         }
         
         
