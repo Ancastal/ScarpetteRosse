d0db8c0cc25f6a3611de2703966e2083de10bc6f
Antonio Castaldo <ancastal@libero.it>
2021-12-06 14:47:28 +0100

    afeawef

---
+++ ScarpetteRossastre/Nuova roba/Resources/Color.swift
@@ -0,0 +1,22 @@
+//
+//  Color.swift
+//  ProjectWomen2
+//
+//  New Custom Color (Blue)
+//  Hex: #59CAFF
+//  Created by Antonio Castaldo on 27/11/21.
+//
+
+import Foundation
+import SwiftUI
+
+let blue = Color("myCustomBlue")
+let bluegrad = Color("Blue_Gradient")
+let purplegrad = Color("Purple_Gradient")
+let red = Color("Red")
+let violet = Color("Violet")
+let yellow = Color("Yellow")
+let lightyellow = Color("Light-Violet")
+let orange = Color("Orange")
+
+
